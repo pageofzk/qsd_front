751d301b912dd8214e69afe2266d1943d6aabfdc
pageofzk <zengkaizy@163.com>
2015-08-08 14:45:08 +0800

    add detail page

--- js/qsd.js
+++ js/qsd.js
@@ -1,9 +1,14 @@
 (function(){
-	var module = angular.module("postMod",[]);
-	module.run(function() {
+	var postsMod = angular.module("postsMod",[]);
+	var postMod = angular.module("postMod",[]);
+	postsMod.run(function() {
 	    AV.initialize("1ootsw3y4smje21veqtkn2w6xtk1al2hdn6xs05vvzp0ed4k", "p0ir4kljnq05g4jm7udb7u2lrk45cy1c5hawkufgtkcck00p");
 	});
-	module.controller("postCtrl",['$http', '$scope', function($http, $scope){
+	postMod.run(function() {
+	    AV.initialize("1ootsw3y4smje21veqtkn2w6xtk1al2hdn6xs05vvzp0ed4k", "p0ir4kljnq05g4jm7udb7u2lrk45cy1c5hawkufgtkcck00p");
+
+	});
+	postsMod.controller("postsCtrl",['$http', '$scope', function($http, $scope){
 	  $scope.posts = [];
 
 
@@ -96,5 +101,81 @@
 
     $scope.getPosts();
     $scope.gethotPosts();
+	}]);
+	postMod.controller("postCtrl",['$http', '$scope', function($http, $scope){
+
+	  $scope.getPost = function(objectId) {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+	  	query.get(objectId, {
+    			success: function(post) {
+				// 成功，回调中可以取得这个 Post 对象的一个实例
+				$scope.$apply(function(){
+	  				$scope.post = JSON.parse(JSON.stringify(post));
+	  			})
+    			},
+    			error: function(object, error) {
+      		// 失败了.
+      		console.log(object);
+    			}
+	  	});
+	  }
+
+	  $scope.updatePostGood = function(postParam) {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+	  	// 可以先查询出要修改的那条存储
+
+			// 这个 id 是要修改条目的 objectId，你在生成这个实例并成功保存时可以获取到，请看前面的文档
+			query.get(postParam.objectId, {
+    			success: function(post) {
+      		// 成功，回调中可以取得这个 Post 对象的一个实例，然后就可以修改它了
+      		post.increment('good');
+      		post.save();
+    			},
+    			error: function(object, error) {
+      		// 失败了.
+      		console.log(object);
+    			}
+	  	});
+	  }
+
+	  $scope.updatePostBad = function(postParam) {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+	  	// 可以先查询出要修改的那条存储
+
+			// 这个 id 是要修改条目的 objectId，你在生成这个实例并成功保存时可以获取到，请看前面的文档
+			query.get(postParam.objectId, {
+    			success: function(post) {
+      		// 成功，回调中可以取得这个 Post 对象的一个实例，然后就可以修改它了
+      		post.increment('bad');
+      		post.save();
+    			},
+    			error: function(object, error) {
+      		// 失败了.
+      		console.log(object);
+    			}
+	  	});
+	  }
+	  
+	  $scope.hotPosts = [];
+
+	  $scope.gethotPosts = function() {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+	  	query.descending("click");
+	  	query.limit(6);
+	  	query.find({
+	  		success:function (results){
+	  			$scope.$apply(function(){
+	  				$scope.hotPosts = JSON.parse(JSON.stringify(results));
+	  			})
+	  		}
+	  	})
+	  }
+	  
+    $scope.getPost(paramMap.get('id'));
+	$scope.gethotPosts();
 	}]);
 })();
