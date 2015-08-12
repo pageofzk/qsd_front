e46e3c261fb50e61edc88d4a2e35053fcd8e8238
pageofzk <zengkaizy@163.com>
2015-08-12 13:40:14 +0800

    add test page

--- js/qsd.js
+++ js/qsd.js
@@ -2,6 +2,7 @@
 	var postsMod = angular.module("postsMod",[]);
 	var postMod = angular.module("postMod",[]);
 	var buysMod = angular.module("buysMod",[]);
+	var testsMod = angular.module("testsMod",[]);
 	var newsMod = angular.module("newsMod",[]);
 	postsMod.run(function() {
 	    AV.initialize("1ootsw3y4smje21veqtkn2w6xtk1al2hdn6xs05vvzp0ed4k", "p0ir4kljnq05g4jm7udb7u2lrk45cy1c5hawkufgtkcck00p");
@@ -12,10 +13,167 @@
 	newsMod.run(function() {
 	    AV.initialize("1ootsw3y4smje21veqtkn2w6xtk1al2hdn6xs05vvzp0ed4k", "p0ir4kljnq05g4jm7udb7u2lrk45cy1c5hawkufgtkcck00p");
 	});
+	testsMod.run(function() {
+	    AV.initialize("1ootsw3y4smje21veqtkn2w6xtk1al2hdn6xs05vvzp0ed4k", "p0ir4kljnq05g4jm7udb7u2lrk45cy1c5hawkufgtkcck00p");
+	});
 	postMod.run(function() {
 	    AV.initialize("1ootsw3y4smje21veqtkn2w6xtk1al2hdn6xs05vvzp0ed4k", "p0ir4kljnq05g4jm7udb7u2lrk45cy1c5hawkufgtkcck00p");
 
 	});
+	
+	testsMod.controller("testsCtrl",['$http', '$scope', function($http, $scope){
+	  $scope.posts = [];
+
+
+	  $scope.getPosts = function() {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+		query.equalTo("doc_type", "test");
+	  	query.limit(10);
+	  	query.descending("time");
+	  	query.find({
+	  		success:function (results){
+	  			$scope.$apply(function(){
+	  				$scope.posts = JSON.parse(JSON.stringify(results));;
+	  			})
+	  		}
+	  	})
+	  }
+
+	  $scope.hotPosts = [];
+
+	  $scope.gethotPosts = function() {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+		query.equalTo("doc_type", "test");
+	  	query.descending("click");
+	  	query.limit(6);
+	  	query.find({
+	  		success:function (results){
+	  			$scope.$apply(function(){
+	  				$scope.hotPosts = JSON.parse(JSON.stringify(results));
+	  			})
+	  		}
+	  	})
+	  }
+
+	  $scope.gswap = false;
+	  $scope.bswap = false;
+	  $scope.cswap = false;
+	  $scope.updatePostGood = function(postParam) {
+		if (!$scope.gswap) {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+	  	// 可以先查询出要修改的那条存储
+			// 这个 id 是要修改条目的 objectId，你在生成这个实例并成功保存时可以获取到，请看前面的文档
+			query.get(postParam.objectId, {
+    			success: function(post) {
+      		// 成功，回调中可以取得这个 Post 对象的一个实例，然后就可以修改它了
+				post.increment('good');
+				post.save();
+				$scope.$apply(function(){
+					postParam.good=postParam.good+1
+					$scope.gswap = true;
+	  			})
+    			},
+    			error: function(object, error) {
+      		// 失败了.
+      		console.log(object);
+    			}
+	  	});
+		}
+	  }
+
+	  $scope.updatePostBad = function(postParam) {
+		if (!$scope.bswap) {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+	  	// 可以先查询出要修改的那条存储
+
+			// 这个 id 是要修改条目的 objectId，你在生成这个实例并成功保存时可以获取到，请看前面的文档
+			query.get(postParam.objectId, {
+    			success: function(post) {
+      		// 成功，回调中可以取得这个 Post 对象的一个实例，然后就可以修改它了
+				post.increment('bad');
+				post.save();
+				$scope.$apply(function(){
+					postParam.bad=postParam.bad+1;
+					$scope.bswap = true;
+	  			})
+    			},
+    			error: function(object, error) {
+      		// 失败了.
+      		console.log(object);
+    			}
+	  	});
+		}
+	  }
+
+	  $scope.updatePostClick = function(postParam) {
+		if (!$scope.cswap) {
+	  	var Post = AV.Object.extend("Post");
+	  	var query = new AV.Query(Post);
+	  	// 可以先查询出要修改的那条存储
+
+			// 这个 id 是要修改条目的 objectId，你在生成这个实例并成功保存时可以获取到，请看前面的文档
+			query.get(postParam.objectId, {
+    			success: function(post) {
+      		// 成功，回调中可以取得这个 Post 对象的一个实例，然后就可以修改它了
+      		post.increment('click');
+      		post.save();
+			$scope.cswap = true;
+    			},
+    			error: function(object, error) {
+      		// 失败了.
+      		console.log(object);
+    			}
+	  	});
+		}
+	  }
+	  
+	var page = 1;                               //读取的页数         
+	$scope.loading = 0;						//判断是否正在读取内容的变量
+	function pushContent() {                    //核心是这个函数，向$scope.posts
+	//添加内容          
+	var Post = AV.Object.extend("Post");
+	var query = new AV.Query(Post);
+	query.equalTo("doc_type", "test");
+	query.descending("time");
+	query.skip(10*page); 
+	query.limit(10);
+	$scope.$apply(function(){
+	if ($scope.loading==0) {                         //如果页面没有正在读取
+		$scope.loading = 1;                     //告知正在读取             
+	  	query.find({						//调用API，读取第几页的内容                 
+	  		success:function (results){
+	  				posts = JSON.parse(JSON.stringify(results));
+					if (posts.length != 0) {
+						for (var i = 0; i <= posts.length - 1; i++) {                         
+							$scope.posts.push(posts[i]);                     
+						}
+						page++;  						//翻页		
+						$scope.loading = 0;        //告知读取结束
+					} else {
+						$scope.loading = 2;        //告知读取完毕
+					}
+					
+	  		},
+			error: function(error) {
+			}
+		})
+	}
+	})
+	}
+	  
+	$(window).on('scroll', function (event) {   //jquery，事件滚动监听         
+		if ($(document).scrollTop() + $(window).height() >= $(document).height() - 200) { //当滚动到页面底部             
+		pushContent();                      //调用向$scope.posts添加内容函数         
+	  }
+	});
+    $scope.getPosts();
+    $scope.gethotPosts();
+	}]);
+	
 	postsMod.controller("postsCtrl",['$http', '$scope', function($http, $scope){
 	  $scope.posts = [];
 
